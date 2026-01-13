dfb814614e2c07631e3867a737d14c7f961495b2
Solanoize <yanwarsolah@gmail.com>
2026-01-13 21:29:44 +0700

    feat: add TestNG configuration and new test classes

---
+++ src/test/java/com/juaracoding/testng/ClassTest01.java
@@ -0,0 +1,16 @@
+package com.juaracoding.testng;
+
+import org.testng.annotations.Test;
+
+public class ClassTest01 {
+
+    @Test
+    public void test01() {
+        System.out.println("ClassTest01->test01()");
+    }
+
+    @Test
+    public void test02() {
+        System.out.println("ClassTest01->test02()");
+    }
+}
---
+++ src/test/java/com/juaracoding/testng/ClassTest02.java
@@ -0,0 +1,16 @@
+package com.juaracoding.testng;
+
+import org.testng.annotations.Test;
+
+public class ClassTest02 {
+
+    @Test
+    public void test01() {
+        System.out.println("ClassTest02->test01()");
+    }
+
+    @Test
+    public void test02() {
+        System.out.println("ClassTest02->test02()");
+    }
+}
